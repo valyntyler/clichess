bbb0c256507a62b9cb28c911219c34c636afc5fd
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-08-31 23:42:11 +0200

    chore: update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 use clichess::board::Board;
-use colored::Colorize;
 
 fn main() {
-    let pawns = "         ";
-    let pieces = "         ";
-
-    println!();
     println!("{}", Board::default());
 }
