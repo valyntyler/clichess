ebc29b11a304ad8aede73379d974ade2efaabae4
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-08-31 22:27:52 +0200

    feat: update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use colored::Colorize;
+
 fn main() {
-    println!("      ");
+    let pawns = "         ";
+    let pieces = "         ";
+
+    println!("{}", pawns.on_blue());
+    println!("{}", pieces.white());
+
+    for j in 0..8 {
+        for i in 0..8 {
+            match (i + j) % 2 == 0 {
+                true => {
+                    print!("{}", "  ".on_white());
+                }
+                false => {
+                    print!("{}", "  ".on_black());
+                }
+            }
+        }
+        println!();
+    }
 }
