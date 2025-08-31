32ccb2500a9dca6fc409b7c27d1636d49a47796f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-08-31 23:41:20 +0200

    feat: update mod piece

--- src/piece/mod.rs
+++ src/piece/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Clone, Copy)]
 pub enum PieceColor {
     White,
@@ -23,5 +25,22 @@ pub struct Piece {
 impl Piece {
     pub fn new(color: PieceColor, shape: PieceShape) -> Self {
         Self { color, shape }
+    }
+}
+
+impl Display for Piece {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self.shape {
+                PieceShape::Pawn => '',
+                PieceShape::Knight => '',
+                PieceShape::Bishop => '',
+                PieceShape::Rook => '',
+                PieceShape::Queen => '',
+                PieceShape::King => '',
+            }
+        )
     }
 }
