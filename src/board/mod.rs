d59580412c06b753af43380a0e994d704b1366a2
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-09-01 00:17:31 +0200

    feat: update mod board

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -1,8 +1,13 @@
 use std::fmt::Display;
 
-use colored::{Color, Colorize};
+use colored::{Color, ColoredString, Colorize};
 
-use crate::piece::{Piece, color::PieceColor, shape::PieceShape};
+use crate::piece::{
+    Piece,
+    char::{NerdFontNotation, PieceChar},
+    color::PieceColor,
+    shape::PieceShape,
+};
 
 pub struct Board([[Option<Piece>; 8]; 8]);
 
@@ -24,26 +29,26 @@ impl Default for Board {
 impl Display for Board {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for (rank, row) in self.0.iter().enumerate() {
-            for (file, piece) in row.iter().enumerate() {
-                let fg = Color::Red;
-
+            for (file, cell) in row.iter().enumerate() {
                 let bg = if (rank + file) % 2 == 0 {
                     Color::BrightBlack
                 } else {
                     Color::White
                 };
 
-                let square = format!(
-                    " {} ",
-                    if let Some(p) = piece {
-                        p.to_string()
-                    } else {
-                        " ".to_string()
-                    }
-                )
-                .color(fg)
-                .on_color(bg);
+                let char = if let Some(piece) = cell {
+                    NerdFontNotation(piece.shape)
+                        .char()
+                        .to_string()
+                        .color(match piece.color {
+                            PieceColor::White => Color::Red,
+                            PieceColor::Black => Color::Blue,
+                        })
+                } else {
+                    ColoredString::from(" ")
+                };
 
+                let square = format!(" {} ", char).on_color(bg);
                 write!(f, "{}", square)?;
             }
             writeln!(f)?;
