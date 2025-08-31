055c819040fe10ddca23c32df52f6fe21ec8e85a
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-09-01 00:05:50 +0200

    chore: update mod piece

--- src/piece/mod.rs
+++ src/piece/mod.rs
@@ -2,8 +2,6 @@ pub mod char;
 pub mod color;
 pub mod shape;
 
-use std::fmt::Display;
-
 use crate::piece::color::PieceColor;
 use crate::piece::shape::PieceShape;
 
@@ -16,22 +14,5 @@ pub struct Piece {
 impl Piece {
     pub fn new(color: PieceColor, shape: PieceShape) -> Self {
         Self { color, shape }
-    }
-}
-
-impl Display for Piece {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self.shape {
-                PieceShape::Pawn => '',
-                PieceShape::Knight => '',
-                PieceShape::Bishop => '',
-                PieceShape::Rook => '',
-                PieceShape::Queen => '',
-                PieceShape::King => '',
-            }
-        )
     }
 }
